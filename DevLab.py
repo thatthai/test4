e5f22ffeeb89814d59cf50fb0b6766e5de207dda
Norhty <thaithat788@gmail.com>
2022-08-03 18:28:47 +0700

    hey

--- DevLab.py
+++ DevLab.py
@@ -1,14 +1,20 @@
-n = int(input())
-a = n
-sum = 0
-while a >= 10:
-    m = n % 10
-    n = n//10
-    sum += m
-    if n < 1 :
-        a = sum
-        if a > 10 :
-            n = a
-            sum = 0
-    
-print(a)
+row = int(input())
+col = (2*row) - 1
+
+n = 1
+m = 1
+
+for i in range(2,col + 1) :
+    while n <= i :
+        while m <= (2*i) - 1:
+            if m >= row - (n-1) and m <= row + (n-1)  :
+                print("*", end = "")
+            else :
+                print(" ", end ="")
+            m += 1
+        n += 1
+        print("")
+        m = 1
+    n = 1
+    m = 1
+   
