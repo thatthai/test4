4060e085312838f2536a641924128bb513cfad1f
Norhty <thaithat788@gmail.com>
2022-08-03 20:03:51 +0700

    character change case

--- DevLab.py
+++ DevLab.py
@@ -1,27 +1,9 @@
-row = int(input())
+mes = input()
+new_mes = ""
 
-
-n = 1
-m = 1
-k = 2
-
-while k <= row + 1 :
-    while n <= k :
-        while m <= (2*(row+1)) - 1:
-            if m >= row+1 - (n-1) and m <= row+1 + (n-1)  :
-                print("*", end = "")
-            else :
-                print(" ", end ="")
-            m += 1
-        n += 1
-        print("")
-        m = 1
-    n = 1
-    m = 1
-    k += 1
-
-print(" " * row , end = "")
-print("|")
-print("=" * row , end = "")
-print("V", end = "")
-print("=" * row)
+for c in mes :
+    if c.isupper() :
+        new_mes += c.lower()
+    else :
+        new_mes += c.upper()
+print(new_mes)
